7601f62f7e04e01bc4f067835c751867d528f7f6
Kdoggg666 <kenanwright@gmail.com>
2022-03-19 16:56:02 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+from datetime import date
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for, send_file)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask_paginate import Pagination, get_page_args
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+@app.route("/")
+@app.route("/home")
+def about():
+    """
+    App route for home page
+    """
+
+    return render_template('about.html')
+
+
+@app.route("/experience")
+def experience():
+    """
+    App route for experience page
+    """
+
+    return render_template('experience.html')
+
+
+@app.route("/portfolio")
+def portfolio():
+    """
+    App route for portfolio page
+    """
+    projects = list(mongo.db.portfolio.find())
+
+    return render_template('portfolio.html', projects=projects)
+
+
+# @app.route('/download')
+# def downloadFile ():
+#     path = "/static/files/Kenan's Resume.pdf"
+#     return send_file(path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
