17844ba0223b2374cbb1afc09691b3fe19493c9e
Kdoggg666 <kenanwright@gmail.com>
2022-05-09 09:24:03 +0000

    Remove contact route and template

--- app.py
+++ app.py
@@ -49,25 +49,6 @@ def portfolio():
     return render_template('portfolio.html', projects=projects)
 
 
-@app.route('/contact', methods=["GET","POST"])
-def get_contact():
-    """
-        Render contact form
-    """
-    form = ContactForm()
-    # here, if the request type is a POST we get the data on contat
-    # forms and save them else we return the contact forms html page
-    if request.method == 'POST':
-        name =  request.form["name"]
-        email = request.form["email"]
-        subject = request.form["subject"]
-        message = request.form["message"]
-        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
-        res.to_csv('./contactusMessage.csv')
-        print("The data are saved !")
-    else:
-        return render_template('contact.html', form=form)
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
